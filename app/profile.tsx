0db48bc3c3242226f2321b5a4e9c17f85b14d556
menda <jmendana@fi.uba.ar>
2025-04-19 01:10:33 -0300

    added name, surname, email fields

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 export default function ProfileScreen() {
@@ -12,6 +12,34 @@ export default function ProfileScreen() {
           style={styles.profileImage}
         />
         <Text style={styles.profileName}>Tuntung Sahur</Text>
+      </View>
+
+      {/* Name Field */}
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Name</Text>
+        <TextInput
+          style={styles.input}
+          value="Tuntung"
+          editable={false} // Set to true if you want it to be editable later
+        />
+      </View>
+      {/* Surname Field */}
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Surname</Text>
+        <TextInput
+          style={styles.input}
+          value="Sahur"
+          editable={false} // Set to true if you want it to be editable later
+        />
+      </View>
+      {/* Surname Field */}
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          value="tuntungsahur@gmail.com"
+          editable={false} // Set to true if you want it to be editable later
+        />
       </View>
     </View>
   );
@@ -30,6 +58,7 @@ const styles = StyleSheet.create({
   profileRow: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginBottom: 20,
   },
   profileImage: {
     width: 80,
@@ -40,9 +69,27 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   profileName: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
     marginStart: 20,
     marginTop: 20,
-  }
+  },
+  inputGroup: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 6,
+  },
+  input: {
+    height: 44,
+    borderColor: '#A8A8A8',
+    color: '#A8A8A8',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    fontSize: 16,
+  },
 });
